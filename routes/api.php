e275d8eb4e3265b87bf56bdc2477a9d0034b7aa1
Socolov <145919428+falcon-ov@users.noreply.github.com>
2025-06-23 23:54:47 +0300

    update: fix auth-api-routes

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,17 @@ use App\Http\Controllers\Api\BookApiController;
 use App\Http\Controllers\Api\AuthorApiController;
 use App\Http\Controllers\Api\CategoryApiController;
 use App\Http\Controllers\Api\BorrowApiController;
+use App\Http\Controllers\AuthController;
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+});
 
 Route::middleware('auth:api')->group(function () {
     Route::resource('books', BookApiController::class);
